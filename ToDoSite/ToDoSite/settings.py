052dbe2c59099f03cb492ebdcb28d853d4610e31
tdb1192@gmail.com <tyler@Tylers-MacBook-Air.local>
2022-09-06 20:34:38 -0400

    Created Dockerfile and updated docker-compose file to allow for full deployment of this application.

--- ToDoSite/ToDoSite/settings.py
+++ ToDoSite/ToDoSite/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@(zh&(z&$&b^7a_m(ld29^q$zw-99uyzp_aq25s78elwxdyv*p'
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY','django-insecure-@(zh&(z&$&b^7a_m(ld29^q$zw-99uyzp_aq25s78elwxdyv*p')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+#DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -80,7 +84,7 @@ DATABASES = {
         'NAME': 'todo',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '5432'
     }
 }
